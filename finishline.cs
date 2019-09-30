33653c6f3e5d4114ab0a5ad3d8c61958917f52e9
andrewbraunsdorf <45677101+andrewbraunsdorf@users.noreply.github.com>
2019-09-30 18:14:32 -0400

    updated finishline

--- finishline.cs
+++ finishline.cs
@@ -1,61 +1,52 @@
 using System;
 using System.Collections.Generic;
 
-// possible classes
-// die X
-// deck X
-// card X
-// player X
-// marker X
-// game or board
-
-public class Die
-{
-  // sides value color
-  public int val;
-  private int sides;
-  public int color;
-
-  public Die(int sides, int color)
-  {
-    this.sides = sides;
-    this.color = color;
-    this.val = 1;
-  }
-  public Die(int sides)
-  {
-    this.sides = sides;
-    this.val = 1;
-  }
-  public Die()
-  {
-    this.sides = 6;
-    this.val = 1;
-  }
-
-
-
-  // modify the existing class
-  public void Roll(Random rand)
-  {
-    this.val = rand.Next(1, this.sides + 1);
-  }
-}
-
-public class Card
+namespace finishLineEvening
 {
 
-  public int suit;
-  public int val;
-  private Dictionary<int, string> suit_Map = new Dictionary<int, string>
-  {
+    public class Die
+    {
+        // sides value color
+        public int val;
+        private int sides;
+        public int color;
+
+        public Die(int sides, int color)
+        {
+            this.sides = sides;
+            this.color = color;
+            this.val = 1;
+        }
+
+        public Die(int sides)
+        {
+            this.sides = sides;
+            this.val = 1;
+        }
+
+        public Die()
+        {
+            this.sides = 6;
+            this.val = 1;
+        }
+
+        public void Roll(Random rand)
+        {
+            this.val = rand.Next(1, this.sides + 1);
+        }
+    }
+
+    public class Card
+    {
+        public int suit;
+        public int val;
+        private Dictionary<int, string> SUIT_MAP = new Dictionary<int, string> {
     {0, "\u2663"},
     {1, "\u2660"},
-    {2, "\u2265"},
+    {2, "\u2665"},
     {3, "\u2666"}
   };
-  private Dictionary<int, string> val_Map = new Dictionary<int, string>
-  {
+        private Dictionary<int, string> VAL_MAP = new Dictionary<int, string> {
     {1, "Ac"},
     {10, "10"},
     {11, "Ja"},
@@ -63,217 +54,296 @@ public class Card
     {13, "Ki"}
   };
 
-  public Card(int val, int suit)
-  {
-    this.val = val;
-    this.suit = suit;
-  }
-
-  public string Display()
-  {
-    if (this.val == 0)
-    {
-      return "Jkr";
-    }
-
-    if (this.valMap.ContainsKey(this.val))
-    {
-      return this.suit_Map[this.suit] + this.val_Map[this.val];
-    }
-
-    return this.suit_Map[this.suit] + "0" + this.val;
-  }
-}
-
-public class Deck
-{
-    public List<Card> cards;
-
-    public Deck(int[] values, int[] suits, int numJokers)
-    {
-        foreach(var suit in suits)
+        public Card(int val, int suit)
         {
-            foreach(var val in values)
+            this.val = val;
+            this.suit = suit;
+        }
+
+        public string Display()
+        {
+            if (this.val == 0)
             {
-                var newCard = new Card(val, suit);
+                return "Jkr";
+            }
+
+            if (this.VAL_MAP.ContainsKey(this.val))
+            {
+                return this.SUIT_MAP[this.suit] + this.VAL_MAP[this.val];
+            }
+
+            return this.SUIT_MAP[this.suit] + "0" + this.val;
+
+        }
+
+    }
+
+    public class Deck
+    {
+        public List<Card> cards = new List<Card>();
+
+        public Deck(int[] values, int[] suits, int numJokers)
+        {
+            foreach (var suit in suits)
+            {
+                foreach (var val in values)
+                {
+                    this.cards.Add(new Card(val, suit));
+                }
+            }
+            for (int jkr = 0; jkr < numJokers; jkr++)
+            {
+                this.cards.Add(new Card(0, 0));
             }
         }
-        for (int jkr = 0; jkr < numJokers; jkr++)
+
+        public void Shuffle(Random rand)
         {
-          this.cards.Add(newCard); 
+            for (int index = this.cards.Count - 1; index > 0; index--)
+            {
+                int position = rand.Next(index + 1);
+                Card temp = this.cards[index];
+                this.cards[index] = this.cards[position];
+                this.cards[position] = temp;
+            }
         }
     }
 
-    public void Shuffle(Random rand)
+    public class Marker
     {
-        for( int index = this.cards.Count - 1; index > 0; index--)
+        public int position;
+        public string name;
+
+
+        public Marker(string name)
         {
-            int position = rand.Next(index + 1);
-            Card temp = this.cards[index];
-            this.cards[index] = this.cards[position];
-            this.cards[position] = temp;
+            this.position = -1;
+            this.name = name;
+        }
+
+        public virtual void Move(int spaces)
+        {
+            this.position += spaces;
         }
     }
-}
-  
-public class Marker
-{
-    public int position;
-    public string name;
-    
-    public Marker(string name)
-    {
-        this.position = -1;
-        this.name = name;
-    }
 
-    public virtual void Move(int spaces)
+    public class FLMarker : Marker
     {
-        this.position += spaces;
-    }
-}
-  
-public class FLMarker : Marker
-{
-    public bool stopped;
-    public FLMarker(string name) : base(name)
-    {
-        this.stopped = false;
-    }
-
-    // parent has class but use this one instead of parents
-    public void Move(int spaces, int stopValue)
-    {
-        // pre-processing
-        this.Move(spaces);
-        // post-processing
-    }
-}
-  
-public class Player
-{
-    public Marker[] markers;
-    public string name;
-  
-    public Player(string name, string[] markerNames)
-    {
-        this.markers = new Marker[markerNames.Length];
-        this.name = name;
-        for (int markerName = 0; markerName < markerNames.Length; markerName++)
+        public bool stopped;
+        public FLMarker(string name) : base(name)
         {
-            this.markers[markerName] = new Marker(markerNames[markerName]);
+            this.stopped = false;
+        }
+
+        public void Move(int spaces, int stopValue, Deck gameDeck)
+        {
+            for (int count = 1; count <= spaces; count++)
+            {
+                if (this.position + count >= gameDeck.cards.Count)
+                {
+                    return;
+                }
+                if (gameDeck.cards[this.position + count].val >= stopValue)
+                {
+                    Move(count);
+                    return;
+                }
+            }
+            // preprocessing
+            this.Move(spaces);
+            // postprocess
         }
     }
-}
 
-  public string HasMarkersAt(int position)
-  {
-    string master = "";
-    foreach (var marker in this.markers)
+    public class Player
     {
-      if (marker.position == position)
-      {
-        master += marker.name;
-      }
-      else
-      {
-        master += " ";
-      }
+        public FLMarker[] markers;
+        public string name;
+
+        public Player(string name, string[] markerNames)
+        {
+            this.markers = new FLMarker[markerNames.Length];
+            this.name = name;
+            for (int markerName = 0; markerName < markerNames.Length; markerName++)
+            {
+                this.markers[markerName] = new FLMarker(markerNames[markerName]);
+            }
+        }
+
+        public string HasMarkersAt(int position)
+        {
+            string master = "";
+            foreach (var marker in this.markers)
+            {
+                if (marker.position == position)
+                {
+                    master += marker.name;
+                }
+                else
+                {
+                    master += " ";
+                }
+            }
+            return master;
+        }
+
+        public int FindMarker(string query)
+        {
+            for (int counter = 0; counter < this.markers.Length; counter++)
+            {
+                if (markers[counter].name == query)
+                {
+                    return counter;
+                }
+            }
+            return -1;
+        }
     }
-    return master;
-  }
-}
 
-public class FinishLine
-{
-    private readonly int[] SUITS = new int[] { 0, 1, 2, 3 };
-    private readonly int[] VALUES = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-    private const int NUM_JOKERS = 2;
-    private readonly string[] MARKER_NAMES = new String[] {"A", "B", "C"};
-
-    public Deck deck;
-    public Die redDie;
-    public Die blackDie;
-    public Player player1;
-    public int players;
-    public Random rand;
-  
-   public FinishLine(int players, string player1Name)
-  {
-    this.players = players;
-    this.player1 = new Player(player1Name, this.MARKER_NAMES);
-    this.rand = new Random();
-    this.deck = new Deck(this.VALUES, this.SUITS, NUM_JOKERS);
-    this.redDie = new Die(6, 0xFF0000);
-    this.blackDie = new Die(6, 0x000000);
-    this.deck.Shuffle(rand);
-    this.redDie.Roll(rand);
-    this.blackDie.Roll(rand);
-  }
-  
-  public void DisplayBoard()
-  {
-    // how to display
-    // \t[SVV]\t[SVV] [SVV] [SVV]
-    // \t_MMM_\t_MMM
-    // ABC
-    // AB
-    // BC
-    // C
-    // A C
-
-    Console.Clear();
-    string master = "";
-    string cardRow = "\t";
-    string playerRow = "\t";
-    int counter = 0;
-    foreach (Card card in this.deck.cards)
+    public class FinishLine
     {
-      cardRow += "|" + card.Display() + "|";
-      playerRow += " " + this.player1.HasMarkersAt(counter) + " ";
-      counter++;
-      if (counter % 9 == 0)
-      {
-        master += cardRow + "\n" + playerRow + "\n\n";
-        cardRow = "\t";
-        playerRow = "\t";
-      }
-      else
-      {
-        cardRow += "\t";
-        playerRow += "\t";
-      }
+        private readonly int[] SUITS = new int[] { 0, 1, 2, 3 };
+        private readonly int[] VALUES = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+        private static int NUM_JOKERS = 2;
+        private readonly string[] MARKER_NAMES = new string[] { "A", "B", "C" };
+        private readonly int[] RESTRICTED_VALUES = new int[] { 0, 1, 2, 11, 12, 13 };
+        public Deck deck;
+        public Die redDie;
+        public Die blackDie;
+        public Player player1;
+        public int players;
+        public Random rand;
+
+        public FinishLine(int players, string player1Name)
+        {
+            this.players = players;
+            this.player1 = new Player(player1Name, this.MARKER_NAMES);
+            this.rand = new Random();
+            this.deck = new Deck(this.VALUES, this.SUITS, NUM_JOKERS);
+            this.redDie = new Die(6, 0xFF0000);
+            this.blackDie = new Die(6, 0x000000);
+            this.deck.Shuffle(rand);
+            ValidateDeck();
+            this.redDie.Roll(rand);
+            this.blackDie.Roll(rand);
+        }
+
+        public void DisplayBoard()
+        {
+
+            Console.Clear();
+            string master = "";
+            string cardRow = "\t";
+            string playerRow = "\t";
+
+            cardRow += "Player1";
+            playerRow += this.player1.HasMarkersAt(-1);
+
+            master += cardRow + "\n" + playerRow + "\n\n";
+            cardRow = "\t";
+            playerRow = "\t";
+
+            int counter = 0;
+            foreach (Card card in this.deck.cards)
+            {
+                cardRow += "|" + card.Display() + "|";
+                playerRow += " " + this.player1.HasMarkersAt(counter) + " ";
+                counter++;
+                if (counter % 9 == 0)
+                {
+                    master += cardRow + "\n" + playerRow + "\n\n";
+                    cardRow = "\t";
+                    playerRow = "\t";
+                }
+                else
+                {
+                    cardRow += "\t";
+                    playerRow += "\t";
+                }
+            }
+            Console.WriteLine(master);
+        }
+
+        public void ValidateCard(int position)
+        {
+            if (Array.IndexOf(RESTRICTED_VALUES, this.deck.cards[position].val) >= 0)
+            {
+                while (true)
+                {
+                    int newPosition = this.rand.Next(3, 51);
+                    if (Array.IndexOf(RESTRICTED_VALUES, this.deck.cards[newPosition].val) >= 0)
+                    {
+                        continue;
+                    }
+                    Card temp = this.deck.cards[position];
+                    this.deck.cards[position] = this.deck.cards[newPosition];
+                    this.deck.cards[newPosition] = temp;
+                    break;
+                }
+            }
+        }
+
+        public void ValidateDeck()
+        {
+            int[] RESTRICTED_POSITIONS = new int[] { 0, 1, 2, 51, 52, 53 };
+            foreach (int position in RESTRICTED_POSITIONS)
+            {
+                ValidateCard(position);
+            }
+
+        }
+
+        public void Turn(Player player)
+        {
+            DisplayBoard();
+            string master = "";
+            master += player.name + "'s turn!\n";
+            this.redDie.Roll(this.rand);
+            this.blackDie.Roll(this.rand);
+            int stopValue = this.redDie.val + this.blackDie.val;
+            master += "Red: " + this.redDie.val + "\tBlack: " +
+                this.blackDie.val + "\tStop Value: " + stopValue + "\n";
+
+            GetMarker("Red", redDie, player, stopValue, master);
+            GetMarker("Black", blackDie, player, stopValue, master);
+        }
+
+        public void GetMarker(string dieName, Die die, Player player, int stopValue, string master)
+        {
+            Console.WriteLine(master);
+            Console.WriteLine("Choose marker (A, B, C) for {0} Die", dieName);
+            string input = Console.ReadLine();
+            int inputIndex = player.FindMarker(input.ToUpper());
+            player.markers[inputIndex].Move(die.val, stopValue, this.deck);
+            DisplayBoard();
+        }
+
+        public void Round()
+        {
+            // TODO: loop through players
+            Turn(player1);
+        }
+
+        public void PlayGame()
+
+        {
+            while(true)
+            {
+                Round();
+                //break;
+            }
+        }
+
+        // if(Player.hasMarkersAt(53) == "ABC") they win
     }
-    Console.WriteLine(master);
-  }
-
-}
-}
-
-public class Program
-{
-  public static void Main()
-  {
-    Die redDie = new Die(6, 0xFF0000);
-    var blackDie = new Die();
-    var rand = new Random();
-
-    Card aCard = new Card(0, -1);
-    Console.WriteLine("The card is {0}", aCard.Display());
-
-    // Console.WriteLine("redDie: " + redDie.val);
-    // Console.WriteLine("blackDie: {0} {0}", blackDie.val);
-    // Console.WriteLine("bigDie: " + bigDie.val);
-
-    // redDie.Roll(rand);
-    // blackDie.Roll(rand);
-    // bigDie.Roll(rand);
 
 
-    // Console.WriteLine("After the roll");
-    // Console.WriteLine("redDie: " + redDie.val);
-    // Console.WriteLine("blackDie: {0} {0}", blackDie.val);
-    // Console.WriteLine("bigDie: " + bigDie.val);
-
-  }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var game = new FinishLine(1, "player1");
+            game.PlayGame();
+        }
+    }
 }
